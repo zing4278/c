0df7be74a97c3eb948c5dc02cbbc433c8f6f02d5
zing4278 <zing4278@gmail.com>
2019-10-04 23:15:19 +0530

    delete

--- road/src/app/directive/change-text.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ChangeTextDirective } from './change-text.directive';
-
-describe('ChangeTextDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ChangeTextDirective();
-    expect(directive).toBeTruthy();
-  });
-});
